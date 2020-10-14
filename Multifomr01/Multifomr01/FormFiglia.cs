f64da31344dedcb2f5eb1ecf8477f47cc52197d4
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2020-10-14 08:43:34 +0200

    modifiche form MDI

---
+++ Multifomr01/Multifomr01/FormFiglia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multifomr01
+{
+    public partial class FormFiglia : Form
+    {
+        public FormFiglia()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFigliadiChi_Click(object sender, EventArgs e)
+        {
+            if (Owner != null) {
+                MessageBox.Show("form figlia di = "+ Owner);
+            }
+        }
+    }
+}
