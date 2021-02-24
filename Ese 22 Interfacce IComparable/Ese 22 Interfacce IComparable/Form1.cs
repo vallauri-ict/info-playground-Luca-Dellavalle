b5cb7bedf816a52d9ed852255e48e88b70acf27c
Luca-Dellavalle <62664083+Luca-Dellavalle@users.noreply.github.com>
2021-02-24 08:23:46 +0100

    Esercizio con interfaccia IComparable

---
+++ Ese 22 Interfacce IComparable/Ese 22 Interfacce IComparable/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ese_22_Interfacce_IComparable
+{
+    public partial class Form1 : Form
+    {
+        Persona[] vect = {new Persona("Luca","Ariaudo","Fossano"),
+                          new Persona("Antonio","Bonelli","Alba"),
+                          new Persona("Samuele","Stefirca","Savigliano")
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            ShowPersona();
+        }
+
+        public void ShowPersona() {
+            string s = "";
+            foreach (var item in vect)
+            {
+                s += item.ToString() + "\n";
+            }
+            MessageBox.Show(s);
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOrdina_Click(object sender, EventArgs e)
+        {
+            Array.Sort(vect);
+            ShowPersona();
+        }
+    }
+}
---
+++ Ese 22 Interfacce IComparable/Ese 22 Interfacce IComparable/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ese_22_Interfacce_IComparable
+{
+    class Persona : IComparable //permette l'ordinamento 
+    {
+        string nome;
+        string cognome;
+        string città;
+
+        public Persona(string nome, string cognome, string città) {
+            this.Nome = nome;
+            this.Cognome = cognome;
+            this.Città = città;
+        }
+
+        public string Nome { get =>nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+        public string Città { get => città; set => città = value; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            Persona p = obj as Persona;
+            return String.Compare(Cognome, p.Cognome, true);
+        }  
+    }
+}
